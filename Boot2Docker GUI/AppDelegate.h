92cff3ead0bc578775294ba10e535967894d912a
rimusz <rmocius@gmail.com>
2014-02-09 22:14:49 +0000

    Added NSUserNotificationCenterDelegate

--- Boot2Docker GUI/AppDelegate.h
+++ Boot2Docker GUI/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate>
 
 @property (strong, nonatomic) IBOutlet NSMenu *statusMenu;
 @property (strong, nonatomic) NSStatusItem *statusItem;
